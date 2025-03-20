2ba40243bb2ef34fcd7f46998691d5d9294bbd06
zeeshanali0039 <zeeshan.ali889@hotmail.com>
2025-03-20 14:53:34 +0500

    update add spacce

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,6 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
-
 export default function Home() {
   return (
     <div>
