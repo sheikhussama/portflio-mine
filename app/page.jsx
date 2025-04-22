2b078d574cbbf08ac97864015631c1ccfbfed205
zeeshanali0039 <zeeshan.ali889@hotmail.com>
2025-04-22 16:48:48 +0500

    update node

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,7 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 export default function Home() {
+  
   return (
     <div>
       <HeroSection />
